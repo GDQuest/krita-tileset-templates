62e9beea2477bc6ec61f57947cfc08e483b1e903
Nathan <nathan@gdquest.com>
2017-05-28 14:40:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Tilemap template for Krita Tiled and Photoshop
+
+Produce your game's levels faster with this top-down tilemap template! It comes with the PSD, TMX and TSX files.
+
+[Imgur](http://i.imgur.com/FJaAxSL.png)
+
+See the releases tab to get all the files.
+
+_Copyright (c) 2017 Nathan Lovato. Licensed under [CC-By 4.0](https://creativecommons.org/licenses/by/4.0/) terms._
+
+## Changelog
+
+**Version 0.1**:
+
+- Zelda-inspired top-down dungeon tileset with rooms and slim paths.
+- Tiled 1.0 template tileset and level files
+- Krita-compatible PSD file, set up for automatic exports with Adobe Generator.
---
+++ template.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="template" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="template-assets/tileset.png" width="512" height="512"/>
+ <terraintypes>
+  <terrain name="mines" tile="9"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="3" terrain="0,0,0,"/>
+ <tile id="4" terrain="0,0,,0"/>
+ <tile id="8" terrain=",0,,0"/>
+ <tile id="9" terrain="0,0,0,0"/>
+ <tile id="10" terrain="0,,0,"/>
+ <tile id="11" terrain="0,,0,0"/>
+ <tile id="12" terrain=",0,0,0"/>
+ <tile id="16" terrain=",0,,"/>
+ <tile id="17" terrain="0,0,,"/>
+ <tile id="18" terrain="0,,,"/>
+</tileset>
